5e40462a498cb7d884d2fe9a3c3c5220dcc9b51a
Kalinda Fraser <43866292+kalifraser@users.noreply.github.com>
2018-10-10 13:23:54 -0700

    Create List.h

---
+++ List.h
@@ -0,0 +1,42 @@
+// CS 101: PA 2
+// Kalinda Fraser
+// klfraser
+
+#ifndef LIST_H_
+#define LIST_H_
+
+#include <stdio.h>
+
+typedef struct ListObj* List;
+
+// Constructors-Destructors
+List newList(void);
+void freeList (List* pL);
+
+//Access Functions
+int length(List L);
+int index(List L);
+int front(List L);
+int back(List L);
+int get(List L);
+int equals(List A, List B);
+
+//Manipulation procedures
+void clear(List L);
+void moveFront(List L);
+void moveBack(List L);
+void movePrev(List L);
+void moveNext(List L);
+void prepend(List L, int data);
+void append(List L, int data);
+void insertBefore(List L, int data);
+void insertAfter(List L, int data);
+void deleteFront(List L);
+void deleteBack(List L);
+void delete(List L);
+
+//Other operations
+void printList(FILE* out, List L);
+List copyList(List L);
+
+#endif /* LIST_H_ */
